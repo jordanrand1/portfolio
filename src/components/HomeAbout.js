aceb695f6815c27cf61c460287146f51ccce3806
Jordan Rand <jorrand@gmail.com>
2018-11-03 15:49:53 -0600

    progress on abouthome

--- src/components/HomeAbout.js
+++ src/components/HomeAbout.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+background-color: #282828;
+display: flex;
+justify-content: space-evenly;
+height: 350px;
+`
+
+const Picture = styled.div`
+width: 50%;
+background-image: url();
+`
+
+const Text = styled.div`
+width: 50%;
+height: 100%;
+color: #D4AF37;
+`
 
 const HomeAbout = () => (
-  <>
-  </>
+  <Container>
+    <Picture></Picture>
+    <Text>Hobbies</Text>
+  </Container>
 )
 
 export default HomeAbout;
