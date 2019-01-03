dbb115bbd281c9f0407081a96e35dd40736d2be6
Jordan Rand <jorrand@gmail.com>
2019-01-03 11:44:38 -0700

    update to project views

--- src/components/ProjectView.js
+++ src/components/ProjectView.js
@@ -11,38 +11,59 @@ class ProjectView extends React.Component {
   state = {
     projectName: '',
     description: '',
+    innerImage: '',
   }
 
-  render () {
+  componentDidMount () {
     var innerImage = ''
+    var description = ''
+    var projectName = ''
     const url = this.props.match.url.split('/')[1]
     switch (url) {
       case 'bo4companion':
+        projectName = 'Black Ops 4 Companion'
         innerImage = bo4
+        description = 'A passion project that allows users to enter in their GamerTag and view their in-game stats. I created a React and React Native application for this project.'
         break
       case 'yield':
+        projectName = 'Yield'
         innerImage = yieldImg
+        description = 'A time/money management application for DevPoint Labs. Users can select the projects they are working on and clock in and out. Admins can add projects, users, update users and their timecards as well as view data based on timecard information.'
         break
       case 'moviedb':
+        projectName = 'Movie DB App'
         innerImage = movie
+        description = 'A one day project to see what I could accomplish with a couple of friends. We used TheMovieDB API to build this simple application to view the most popular movies, tv shows and actors.'
         break
       case 'flashcard':
+        projectName = 'Flashcard App'
         innerImage = flashCard
+        description = 'A passion project that will utilize machine learning to maximize learning proficiency.'
         break
       default: 
         innerImage = 'none'
+        description = 'none'
     }
-    console.log(innerImage)
+    this.setState({
+      projectName,
+      description,
+      innerImage,
+    })
+  }
+
+  render () {
     return (
       <>
         <Link to="/" className="back-button">Back</Link>
         <div className="devices">
           <div className="mac">
-            <img src={innerImage} className="inner-image"/>
-            <h1>{this.state.projectName}</h1>
-            <h1>Stack: </h1>
-            <h1>About</h1>
+            <img src={this.state.innerImage} className="inner-image"/>
           </div>
+        </div>
+        <div className="container">
+          <h1 className="title-text">{this.state.projectName}</h1>
+          <h1 className="body-text">Stack: </h1>
+          <h1 className="body-text">Description: <br/> {this.state.description}</h1>
         </div>
       </>
     )
